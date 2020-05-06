603ba83ce9de4ff8d064d93086b91c7cd2b9734b
jai guru <jai guru@mydocument>
2020-05-06 18:43:34 +0530

    changes some lines of code

--- FirstRepository/src/mypack/GitRepo.java
+++ FirstRepository/src/mypack/GitRepo.java
@@ -5,6 +5,9 @@ public class GitRepo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      System.out.println("First git Repo");
+     
+     
+     System.out.println("New Feature intriduced");
 	}
 
 }
